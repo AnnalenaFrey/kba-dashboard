178066832e953df9d8e2cfc27d717ae4ef32d60f
Annalena Frey <annalena-frey1@web.de>
2026-08-24 17:23:44 +0200

    switch to config.yml

--- src/kba_dashboard/__main__.py
+++ src/kba_dashboard/__main__.py
@@ -1,25 +1,30 @@
 from .scraper import KBAScraper
-from .config import fz11, BASE_URL, connection_string
+from .config import load_config
 from .storage import LocalStorage
 from pathlib import Path
-from .models.pydantic_models import KBAFile
 from .database import PostgresAdapter
-
+from dotenv import load_dotenv
+from .models.pydantic_models import Product
 
 
 def main() -> None:
+    load_dotenv()
+    config = load_config()
+    print(config)
+
+
     client = KBAScraper()
     storage = LocalStorage(Path("downloads"))
-    db = PostgresAdapter(connection_string=connection_string)
+    db = PostgresAdapter(connection_string=config["postgres_connection_string"])
+
     db.open()
 
-    files = client.discover_files(fz11)
+    files = client.discover_files(Product(**config["products"]["fz11"]))
 
     for file in files:
-        file.storage_path = client.download_file(BASE_URL, file, storage)
+        file.storage_path = client.download_file(config["base_url"], file, storage)
         db.save_raw_document(file)
 
     db.close()
-
 
 main()
